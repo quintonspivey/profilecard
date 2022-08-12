7f3c4b1ee5ae56cacae88eaba24a13e7aacdc4f1
qsbugsy <49853219+qsbugsy@users.noreply.github.com>
2022-08-12 11:59:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import brady from './bradypatriots.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="profile">
+     
+        <img src={brady} alt='treyyoung'  />
+        <p>Team: Patriots</p>
+        <p>Position: Quarterback</p>
+        <p>Conference: AFC</p>
+        
+      
+        
+      </div>
+      
+    
+  );
+}
+
+export default App;
